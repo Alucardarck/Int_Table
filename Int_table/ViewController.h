eed98141c4b8b9ab25661cbecc5247d3610af549
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-06-17 18:31:29 -0500

    Initial Commit

---
+++ Int_table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Int_table
+//
+//  Created by Martin Leon Preciado on 17/06/16.
+//  Copyright © 2016 MartinLP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Int_table/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Int_table
+//
+//  Created by Martin Leon Preciado on 17/06/16.
+//  Copyright © 2016 MartinLP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
